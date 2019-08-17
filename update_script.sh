69071b50129a943828d97ff04b9a6e4e89e185e5
Jeanne Sauron <jsauron@student.42.fr>
2019-08-17 13:46:23 +0200

    Update update_script.sh

--- update_script.sh
+++ update_script.sh
@@ -1,10 +1,2 @@
-# !/bin/bash
-
-s1=$(/usr/bin/cat /etc/crontab_cpy)
-s2=$(/usr/bin/ls -l /etc/crontab)
-if [ "$s1" != "$s2" ]
-	then
-	sudo sendmail root@debian < /etc/init.d/cron_mail.txt
-	diff -u /etc/crontab_cpy /etc/crontab >> /etc/contab_diff.diff
-	ls -l /etc/crontab > /etc/crontab_cpy
-fi
+#! /bin/bash
+apt-get update && apt-get upgrade -y
